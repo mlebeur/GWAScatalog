a78f3d9c1bb0268dbf7eb683a33c7137f1ef271d
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-26 15:47:03 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,17 +3,17 @@ import numpy as np
 from biothings.utils.dataload import dict_convert, dict_sweep
 from biothings import config
 logging = config.logger
-def load_Exac(data_folder):
-    infile = os.path.abspath("/opt/biothings/GRCh37/ExAC/r1/Exac.tsv")
+def load_GWAScatalog(data_folder):
+    infile = os.path.abspath("/opt/biothings/GRCh37/GWAS_catalog/GWAS_catalog.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+ #       _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
         process_key = lambda k: k.replace(" ","_").lower()
         rec = dict_convert(rec,keyfn=process_key)
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
     for _id,docs in results.items():
-        doc = {"_id": _id, "Exac" : docs}
+        doc = {"_id": _id, "GWAScatalog" : docs}
         yield doc
